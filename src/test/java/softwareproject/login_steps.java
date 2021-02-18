a4c99c337d695c0590cae561471e79df715053ab
HP <HP@DESKTOP-KVGN8BM.mynet>
2021-02-18 19:03:06 +0200

    syntax

--- src/test/java/softwareproject/login_steps.java
+++ src/test/java/softwareproject/login_steps.java
@@ -12,32 +12,35 @@ public class login_steps {
 	public login_steps(login_code f) {
 		this.f=f;
 	}
-	@Given("adminstrator has account already")
-	public void adminstrator_has_account_already() {
-	    this.flag=true;
+	@Given("the adminstrator opened the login page")
+	public void the_adminstrator_opened_the_login_page() {
+		 this.flag=true;
 	}
-	@When("he enters {string} and {string} correctly")
+	@When("he enters username {string} and  password {string} correctly")
 	public void he_enters_and(String username, String password) {
 		flag2=  f.checkvalid(username,password);
+	    
 	}
+	
 
 	
 
-
-	@Then("he logs in successfully")
-	public void he_logs_in_successfully() {
-	   assertTrue(flag==true);
+	@Then("he logs in successfully to the website")
+	public void he_logs_in_successfully_to_the_website() {
+		 assertTrue(flag==true);
 	}
-
-	@When("he enters {string} and {string} wrong password")
-	public void he_enters_and_wrong_password(String string, String string2) {
-		
-			flag=  f.checkvalid(string,string2);
-		}
 	
 
-	@Then("he can not log in")
-	public void he_can_not_log_in() {
-		 assertTrue(flag2==false);
+	
+	@When("he enters correct username {string} and  wrong password {string}")
+	public void he_enters_correct_username_and_wrong_password(String string, String string2) {
+		flag=  f.checkvalid(string,string2);
 	}
+
+	
+	@Then("he can not log in  to the website")
+	public void he_can_not_log_in_to_the_website() {
+		assertTrue(flag2==false);
+	}
+
 }
