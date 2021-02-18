cff0f7e93e62a628fde21e3a7b26ca616a6c5f5d
HP <HP@DESKTOP-KVGN8BM.mynet>
2021-02-18 18:51:01 +0200

    admin_login

---
+++ src/main/java/softwareproject/login_code.java
@@ -0,0 +1,45 @@
+package softwareproject;
+
+import java.util.ArrayList;
+
+public class login_code {
+public ArrayList  <structure>  admin ;
+boolean flag2=false;
+boolean flag3=false;
+boolean flag4=false;
+public login_code() {
+	admin=new ArrayList<structure>();
+	structure dd = new structure ("A" , "A");
+	
+admin.add(dd);
+
+} 
+
+public boolean checkvalid( String username,String password) {
+	
+	for (int i=0;i<admin.size();i++) {
+		if (admin.get(i).usernme.equals(username))
+		{
+		
+			
+			flag2=true;
+			if (admin.get(i).password.equals(password)) {
+				flag3=true;
+			}
+		}
+	}
+	if(flag2==true&& flag3==true) {
+		System.out.println("successfully logged in!!");
+		flag4=true;
+	}
+	if(flag2==true&& flag3==false) {
+		System.out.println("wrong password!!");flag4=false;
+	}
+	if(flag2==false) {
+		System.out.println("not correct username!!");flag4=false;
+	}
+	return flag4;
+	
+}
+	
+}
---
+++ src/main/java/softwareproject/structure.java
@@ -0,0 +1,13 @@
+package softwareproject;
+
+public class structure {
+	String usernme;
+	String password;
+
+	public structure(String string, String string2) {
+		usernme= string;
+		password=string2;
+		
+	}
+	
+}
