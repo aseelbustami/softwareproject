0ef9b604b84a0a15b2909e3644569f67e979ff36
HP <HP@DESKTOP-KVGN8BM.mynet>
2021-02-23 21:41:56 +0200

    congiguration_file

---
+++ src/test/java/softwareproject/configuration.java
@@ -0,0 +1,14 @@
+package softwareproject;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.CucumberOptions.SnippetType;
+@RunWith(Cucumber.class)
+@CucumberOptions(features="use_case",plugin= {"html:target/cucumber/wikipedia.html"},
+monochrome=true,snippets=SnippetType.CAMELCASE,glue= {"softwareproject"},strict=true)
+public class configuration {
+
+
+}
