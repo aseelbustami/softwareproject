7f9f860a92c79203e885444b496c73a2847ddc7e
HP <HP@192.168.1.101>
2021-03-10 00:58:03 +0200

    10-3

---
+++ src/main/java/softwareproject/structure2.java
@@ -0,0 +1,12 @@
+package softwareproject;
+
+public class structure2 {
+	String usernme;
+	String password;
+boolean loggged;
+	public structure2(String string, String string2,boolean logged) {
+		usernme= string;
+		password=string2;
+		loggged=loggged;
+	}
+}
