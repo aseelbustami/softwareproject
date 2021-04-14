eb0547a0c48de8323681c3e6780c2e870eba79d6
HP <HP@DESKTOP-KVGN8BM.mynet>
2021-04-14 15:24:11 +0300

    14/4

--- src/main/java/softwareproject/AddBook.java
+++ src/main/java/softwareproject/AddBook.java
@@ -2,8 +2,6 @@ package softwareproject;
 
 
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
 
@@ -57,8 +55,7 @@ book dd77= new book ("0241334144"," becoming  " ,"Michelle Obama","Obama2018" );
 	else {
 		
 		
-		Logger logger = Logger.getLogger(AddBook.class.getSimpleName());
-		logger.log(Level.INFO, "not valid isbn please be carefull");
+		
 	    return false;
 	    }
 		}
