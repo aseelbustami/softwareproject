23c94259c1886a543ba2fce5b9763d61c06bcb6d
HP <HP@DESKTOP-KVGN8BM.mynet>
2021-04-14 21:30:16 +0300

    14/4

--- src/main/java/softwareproject/add_book.java
+++ src/main/java/softwareproject/add_book.java
@@ -1,63 +1,40 @@
 package softwareproject;
-
 import java.util.ArrayList;
-
 public class add_book {
-	
 	private   ArrayList  <book>  books ;
-	 
-	public add_book() {
-	
-		books=new ArrayList<book>();
-		
+	 public add_book() {
+	books=new ArrayList<book>();
 		book dd5= new book ("1524763160" ," promised land  "," Barack Obama  " ,"Obama2020 " );
-		
 		books.add(dd5);
 		book dd66= new book ("0099511029"," educated  " ,"Tara Westover","Westover2018" );
-		
-		
 		books.add(dd66);
 book dd77= new book ("0241334144"," becoming  " ,"Michelle Obama","Obama2018" );
-		
-		
 		books.add(dd77);
-		
 	}
-	
-  public boolean add(String title, String isbn, String author, String signature) {
+	 public boolean add(String title, String isbn, String author, String signature) {
 	int x;
 	    int y=Integer.parseInt(isbn);
 	    boolean  falg = chech_isbn(y);
-	
 	if(falg) {
 		for (int i =0 ; i < books.size() ;i++)
 		{
 			x=Integer.parseInt(books.get(i).get_isbn());
 			if (((x)==(y) )|| (books.get(i).get_signature().equals(signature))) {
 			falg=false;
-				
-			                                       }
+			}
 		}
 		}
-	
 		if(falg) {
 		book dd6= new book (isbn , title,author,signature);
-
-        books.add(dd6);
-
-        return true;
-                }
-		
-	else {
-		
+ books.add(dd6);
+ return true;
+               }
+		else {
 		System.out.println("not valid isbn please be carefull ");
-		
-	    return false;
+		 return false;
 	    }
 		}
-	
-
-private boolean chech_isbn(int y) {
+	private boolean chech_isbn(int y) {
 	int u;
 	int result=0;
 	int i=1;
@@ -67,85 +44,49 @@ private boolean chech_isbn(int y) {
 		i++;
 		y/=10;
 		}
-	
 	if(i>11)return false;
 	else {
 		if(result%11==0  )return true;
 		else return false;}
 }
-
-
-
 public  ArrayList  <book> search_by_titel(String sSSS) {
-	 
-	ArrayList  <book>  books22  = new ArrayList  <book>() ;
+	 ArrayList  <book>  books22  = new ArrayList  <book>() ;
 for (int i =0 ; i < books.size() ;i++)
 {
 	if (books.get(i).get_title().toUpperCase().contains(sSSS.trim().toUpperCase())) {
 	books22.add(books.get(i)) ;
-		
 	}
-		
-
-
-}
+		}
 return books22;
 }
-
-
-
-
 public  ArrayList  <book> search_by_auther(String sSSS) {
-	 
-	ArrayList  <book>  books22  = new ArrayList  <book>() ;
+	 ArrayList  <book>  books22  = new ArrayList  <book>() ;
 for (int i =0 ; i < books.size() ;i++)
 {
 	if (books.get(i).get_author().toUpperCase().contains(sSSS.trim().toUpperCase())) {
 	books22.add(books.get(i)) ;
-		
 	}
-		
-
-
-}
+		}
 return books22;
 }
-
-
 public ArrayList  <book> search_by_ISBN(String sSSS) {
-	 
-	ArrayList  <book>  books22  = new ArrayList  <book>() ;
+	 	ArrayList  <book>  books22  = new ArrayList  <book>() ;
 for (int i =0 ; i < books.size() ;i++)
 {
 	if ((books.get(i).get_isbn()).toUpperCase().contains(sSSS.trim().toUpperCase())) {
 	books22.add(books.get(i)) ;
-		
-	}
-		
-
-
-}
+		}
+		}
 return books22;
 }
 public  ArrayList  <book> search_by_signature(String sSSS) {
-	 
-	ArrayList  <book>  books22  = new ArrayList  <book>() ;
+	 ArrayList  <book>  books22  = new ArrayList  <book>() ;
 for (int i =0 ; i < books.size() ;i++)
 {
 	if (books.get(i).get_signature().toUpperCase().contains(sSSS.trim().toUpperCase())) {
 	books22.add(books.get(i)) ;
-		
-	}
-		
-
-
-}
+		}
+		}
 return books22;
 }
-
-
-
-
-
-
 }
