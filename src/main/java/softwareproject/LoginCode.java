b5e0e9b47fab5bea0818cc93433496484e2636d7
HP <HP@DESKTOP-KVGN8BM.mynet>
2021-04-21 11:35:06 +0300

    21/4

--- src/main/java/softwareproject/LoginCode.java
+++ src/main/java/softwareproject/LoginCode.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 public class LoginCode {
 private ArrayList  <TheAdmin>  admin ;
 private ArrayList  <TheAdmin>  user ;
-
 public LoginCode() {
 	admin=new ArrayList<TheAdmin>();
 	TheAdmin dd = new TheAdmin ("A" , "A",false);
@@ -13,58 +12,25 @@ public LoginCode() {
 	TheAdmin dda = new TheAdmin ("s" , "s",false);
 admin.add(dd);
 user.add(dda); 
-
 } 
-
-
 public boolean checkvalidAdmin( String username,String password) {
-	
-	return validAdmin(username, password, true);
-	
-}
+		return validAdmin(username, password, true, admin);}
 
-
-private boolean validAdmin(String username, String password, boolean f) {
-	for (int i=0;i<admin.size();i++) {
-		if ((admin.get(i).usernme.equals(username))&&
-				(admin.get(i).password.equals(password)))
-		{
-			
-				
-				admin.get(i).loggged=f;
-				return true;
-			
-		}
-		
-	}
-
-	return false ;
-}
 public boolean logout( String username,String password) {
-	
-	return validAdmin(username, password, false);
-	
-}
+	return validAdmin(username, password, false, admin);}
+
 public boolean checkvalidUser( String username,String password) {
-	
-	
-	for (int i=0;i<user.size();i++) {
-		if ((user.get(i).usernme.equals(username))&&
-				(user.get(i).password.equals(password)))
-		{
-			
-				
-				user.get(i).loggged=true;
-				return true;
-			
-		}
-		
+	return validAdmin(username, password, true, user);
 	}
 
-	return false ;
-	
-}
-
-
-	
+private boolean validAdmin(String username, String password, boolean f, ArrayList<TheAdmin> array) {
+	for (int i=0;i<array.size();i++) {
+		if ((array.get(i).usernme.equals(username))&&
+				(array.get(i).password.equals(password)))
+		{
+				array.get(i).loggged=f;
+				return true;		}
+	}
+return false ;
+}	
 }
